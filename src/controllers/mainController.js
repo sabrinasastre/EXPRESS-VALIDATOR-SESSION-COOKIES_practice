6b39632a170ae56b52bdb62c0da3502c2d217b88
sabrinadanielasastre@gmail.com <sabrinadanielasastre@gmail.com>
2022-02-19 00:41:00 -0300

    agregué array de objetos en ruta, lógica de error en mainControler, y ejs en index

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,20 +1,23 @@
-
+const { validationResult } = require("express-validator");
+//acá el profe exportó de una manera loca que no entiendo bien por qué:
 module.exports = {
-    index (req, res) {
-        res.render('index');
-    },
-    store (req, res) {
-
-        //guardamos los errors del validationResult(req) en una constante
-
-        //Preguntamos si esta vacio errors
-
-        //si hay errores mandar todos los errores al ejs y mostrarlas
-        
-        
-        //si no vienen errros renderiza la vista index con el mensaje y
-        //los valores que hayan puesto en el formulario
-
-    
+  index(req, res) {
+    res.render("index");
+  },
+  store(req, res) {
+    //guardamos los errors del validationResult(req) en una constante:
+    const errors = validationResult(req);
+    console.log(errors);
+    //Preguntamos si esta vacio errors:
+    if (!errors.isEmpty()) {
+      return res.render("index", { errors: errors.errors, old: req.body });
     }
-}
+    //si hay errores mandar todos los errores al ejs y mostrarlas:
+    res.render("index", { data: req.body });
+
+    //si no vienen errros renderiza la vista index con el mensaje y
+    //los valores que hayan puesto en el formulario
+  },
+};
+
+//module.exports=controller  (lo comenté porque el profe ya exportó de otra manera en línea 3)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+//requiero al "check" que viene en express validator con un destructuring:
+const { check } = require("express-validator");
+const controller = require("../controllers/mainController");
 
-const controller = require('../controllers/mainController');
+//creo un array de validaciones:
+const validaciones = [
+  check("name").notEmpty().withMessage("El nombre no puede estar vacío").bail(),
+  check("color").notEmpty().withMessage("El color no puede estar vacío").bail(),
+  check("email")
+    .notEmpty()
+    .withMessage("El email no puede estar vacío")
+    .bail()
+    .isEmail()
+    .withMessage("ingresar un mail válido"),
+];
 
-router.get('/', controller.index);
-router.post('/', controller.store);
+router.get("/", controller.index);
+router.post("/", validaciones, controller.store);
 
 module.exports = router;
