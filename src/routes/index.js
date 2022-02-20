ad6708aaa9ad0214acfc10a86a8bd2f580119432
sabrinadanielasastre@gmail.com <sabrinadanielasastre@gmail.com>
2022-02-19 21:07:25 -0300

    agregué código en rutas y controlador para session C26

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const { check } = require("express-validator");
 const controller = require("../controllers/mainController");
 
 //creo un array de validaciones:
-const validaciones = [
+/*const validaciones = [
   check("name").notEmpty().withMessage("El nombre no puede estar vacío").bail(),
   check("color").notEmpty().withMessage("El color no puede estar vacío").bail(),
   check("email")
@@ -14,9 +14,25 @@ const validaciones = [
     .bail()
     .isEmail()
     .withMessage("ingresar un mail válido"),
-];
+]; */
+
+/* GET home page. */
+router.get("/", controller.index);
+router.post(
+  "/",
+  [
+    check("name").isLength({ min: 1 }).withMessage("Debe ingresar un nombre"),
+    check("email").isEmail().withMessage("Debe ingresar un email valido"),
+    check("color")
+      .isLength({ min: 1 })
+      .withMessage("Debe seleccionar un color"),
+  ],
+  controller.store
+);
 
 router.get("/", controller.index);
 router.post("/", validaciones, controller.store);
+router.get("/color", controller.color);
+router.get("/borrar", controller.borrar);
 
 module.exports = router;
